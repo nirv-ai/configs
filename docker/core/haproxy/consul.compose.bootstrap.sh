2e8d75a86e5ba397ca17b1dbca9e4972f413566a
noahehall <noahehall@users.noreply.github.com>
2023-01-15 21:41:27 -0700

    consul + haproxy integration

--- docker/core/haproxy/consul.compose.bootstrap.sh
+++ docker/core/haproxy/consul.compose.bootstrap.sh
@@ -17,13 +17,15 @@ EOF
 
 chown -R consul:consul /opt/consul
 
-echo "intiating consul agent"
-su -g consul - consul sh -c "consul agent -node=core-proxy -config-dir=/opt/consul/config" &
-echo "consul success?: $?"
-echo "consul pid saved: : $(cat /opt/consul/pid.consul)"
-
 echo "starting envoy service"
+# agent: Check socket connection failed: check=service:core-proxy-1-sidecar-proxy:1 error="dial tcp 127.0.0.1:21000: connect: connection refused"
+# su -g consul - consul sh -c "consul connect envoy -sidecar-for core-proxy-1" &
 su -g consul - consul sh -c "cd /opt/consul/envoy && envoy -c envoy.yaml" &
 echo "envoy success?: $?"
 echo $! >/opt/consul/pid.envoy
 echo "envoy pid saved: $(cat /opt/consul/pid.envoy)"
+
+echo "starting consul agent"
+su -g consul - consul sh -c "consul agent -node=core-proxy -config-dir=/opt/consul/config" &
+echo "consul success?: $?"
+echo "consul pid saved: : $(cat /opt/consul/pid.consul)"
