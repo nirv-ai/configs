18a9497dfabf41658a210a6d490637f817d83543
noahehall <noahehall@users.noreply.github.com>
2023-01-29 00:56:55 -0700

    core-consul docker 12factor for noamd integration

--- consul/global/config.global.tls.hcl
+++ consul/global/config.global.tls.hcl
@@ -4,10 +4,6 @@ tls {
   }
 
   defaults {
-    // ca_file   = "/run/secrets/consul_ca.pem" # CONSUL_CACERT
-    // cert_file = "/run/secrets/consul_server.pem" # CONSUL_CLIENT_CERT
-    // key_file  = "/run/secrets/consul_server_privkey.pem" # CONSUL_CLIENT_KEY
-
     # @see https://discuss.hashicorp.com/t/access-consul-web-interface-with-verify-incoming-true/39121
     # make sure to create a p12 cert via script.ssl.sh create p12
     # and install it in your browser
--- docker/core/consul/consul.compose.bootstrap.sh
+++ docker/core/consul/consul.compose.bootstrap.sh
@@ -5,25 +5,70 @@
 # uncomment to prevent unhealthy checks and allow you to ssh in and debug
 # sleep 3650d
 
+CBD=${CONSUL_DIR_BASE}          # consul base dir
+CFD=${CBD}/${CONSUL_DIR_CONFIG} # consul config dir
+CDD=${CBD}/${CONSUL_DIR_DATA}   # consul data dir
+
+# re-export things
+# we renamed them for sorting alphabetically
+export CONSUL_HTTP_ADDR=https://${MESH_HOSTNAME}:${CONSUL_PORT_CUNT}
+export CONSUL_TLS_SERVER_NAME=${MESH_SERVER_HOSTNAME}
+
 # let the server start without checking for tokens
 # as we may be bootstrapping a greenfield server
-cat <<-EOF >/consul/config/env.token.hcl
-    acl {
-      tokens {
-        agent  = "$CONSUL_HTTP_TOKEN"
-        default  = "$CONSUL_DNS_TOKEN"
-      }
+# we set auto-reload-config to true so DO NOT remove this
+cat <<-EOF >${CFD}/env.token.hcl
+  acl {
+    tokens {
+      agent  = "$CONSUL_HTTP_TOKEN"
+      default  = "$CONSUL_DNS_TOKEN"
     }
+  }
 EOF
 
-chown -R consul:consul /consul
+# create cert defaults from env vars
+cat <<-EOF >${CFD}/env.certs.hcl
+  tls {
+    defaults {
+      ca_file = "${CONSUL_CACERT}"
+      cert_file = "${CONSUL_CLIENT_CERT}"
+      key_file = "${CONSUL_CLIENT_KEY}"
+    }
+  }
+EOF
 
 start_consul() {
+  # check the docs for additional options
+  # TODO: these need additional logic in place
+  # -encrypt=CONSUL_ENCRYPT_KEY # i think this is set in .env.auto
+  # ensure -ui is only set on server with index 0 or something
+
+  # start the server agent
   consul agent \
-    -config-dir=/consul/config \
-    -data-dir=/consul/data \
+    -advertise='{{ GetInterfaceIP "eth0" }}' \
+    -alt-domain=${CONSUL_ALT_DOMAIN} \
+    -auto-reload-config \
+    -bind=${CONSUL_ADDR_BIND} \
+    -client=${CONSUL_ADDR_CLIENT} \
+    -config-dir=${CFD} \
+    -data-dir=${CDD} \
+    -datacenter=${DATACENTER} \
+    -dns-port=${CONSUL_PORT_DNS} \
     -domain=${MESH_HOSTNAME} \
-    -node=$CONSUL_NODE_PREFIX-$(hostname)
+    -grpc-port=-1 \
+    -grpc-tls-port=${CONSUL_PORT_GRPC} \
+    -http-port=-1 \
+    -https-port=${CONSUL_PORT_CUNT} \
+    -node=${CONSUL_NODE_PREFIX}-$(hostname) \
+    -pid-file=${CBD}/${CONSUL_PID_FILE} \
+    -retry-join=${CONSUL_SERVER} \
+    -serf-lan-bind=${CONSUL_ADDR_BIND_LAN} \
+    -serf-lan-port=${CONSUL_PORT_SERF_LAN} \
+    -serf-wan-bind=${CONSUL_ADDR_BIND_WAN} \
+    -serf-wan-port=${CONSUL_PORT_SERF_WAN} \
+    -server \
+    -server-port=${CONSUL_PORT_SERVER} \
+    -ui
 }
 
 start_consul
